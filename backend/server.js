9bfb9d63a36cb6837f98294b6702c4b5d6c8b4c9
sandeepkumar23f <sandeep21000552@gmail.com>
2025-10-16 23:29:37 +0530

    change in cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://track-your-expense-r8m6.onrender.com/",
     credentials: true,
   })
 );
